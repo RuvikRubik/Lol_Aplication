cdba17695ff546f18ad89e8b6894d38cc1017555
Bartosz Kolberg <falconkrwisty@gmail.com>
2026-01-03 19:31:49 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lol_Aplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lol_Aplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Json
+        {
+            [JsonPropertyName("data")]
+            public Dictionary<string, Champion> champions { get; set; }
+        }
+
+        public class Champion
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public List<string> tags { get; set; }
+            public Stats stats { get; set; }
+
+            public string TagsText => string.Join(", ", tags);
+
+            public string ImageUrl { get; set; }
+        }
+
+        public class Stats
+        {
+            public float hp { get; set; }
+            public float movespeed { get; set; }
+            public float attackdamage { get; set; }
+
+        }
+
+        string ChampionsJSON;
+        string versionJSON;
+        string selectedVersion; // aktualnie wybrana wersja gry
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetVersions();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        // Pobiera danych championów z Data Dragon i tworzenie listy
+        private void GenerateChapions(object sender, SelectionChangedEventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            selectedVersion = VersionsComboBox.SelectedItem.ToString();
+            ChampionsJSON = webClient.DownloadString($"https://ddragon.leagueoflegends.com/cdn/{selectedVersion}/data/en_US/champion.json");
+            var root = JsonSerializer.Deserialize<Json>(ChampionsJSON);
+
+            ChampionsList.ItemsSource = root.champions.Values;
+            foreach (var champ in root.champions.Values)
+            {
+                champ.ImageUrl = $"https://ddragon.leagueoflegends.com/cdn/{selectedVersion}/img/champion/{champ.id}.png";
+            }
+        }
+
+        // Pobranie wersji gry League of Legends
+        private void GetVersions()
+        {
+            WebClient webClient = new WebClient();
+            versionJSON = webClient.DownloadString("https://ddragon.leagueoflegends.com/api/versions.json");
+            List<string> versions = JsonSerializer.Deserialize<List<string>>(versionJSON);
+
+            VersionsComboBox.ItemsSource = versions;
+        }
+
+        private void TextButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetTextMode();
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetImageMode();
+        }
+
+        // Funkcja zmiany trybu wyswietlana postaci na textowy
+        private void SetTextMode()
+        {
+            ChampionsList.ItemTemplate = (DataTemplate)Resources["TextTemplate"];
+            var panelTemplate = new ItemsPanelTemplate(new FrameworkElementFactory(typeof(StackPanel)));
+            ChampionsList.ItemsPanel = panelTemplate;
+        }
+
+        // Funkcja zmiany trybu wyswietlana postaci na graficzny
+        private void SetImageMode()
+        {
+            ChampionsList.ItemTemplate = (DataTemplate)Resources["ImageTemplate"];
+            var factory = new FrameworkElementFactory(typeof(UniformGrid));
+            factory.SetValue(UniformGrid.ColumnsProperty, 3);
+            ChampionsList.ItemsPanel = new ItemsPanelTemplate(factory);
+        }
+
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetTextMode();
+            VersionsComboBox.SelectedIndex = 0;
+        }
+    }
+}
